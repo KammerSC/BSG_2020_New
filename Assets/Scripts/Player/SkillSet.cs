9cb139cacd5423b1a14ddcab3e4f8f69549ee1da
David Borbely <david.borbely.00@gmail.com>
2020-07-23 23:00:28 +0200

    Character and SkillSet classes added.

---
+++ Assets/Scripts/Player/SkillSet.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Player
+{
+    public class SkillSet 
+    {
+        Dictionary<SkillCard.Type, int> skillsets = new Dictionary<SkillCard.Type, int>();
+
+    }
+}
