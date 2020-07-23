8b4f4051fdbc16391fbb3f2ee1dd595628800d9a
David Borbely <david.borbely.00@gmail.com>
2020-07-24 00:17:44 +0200

    Skillset class basic implementation.

--- Assets/Scripts/Player/SkillSet.cs
+++ Assets/Scripts/Player/SkillSet.cs
@@ -5,7 +5,37 @@ namespace Player
 {
     public class SkillSet 
     {
-        Dictionary<SkillCard.Type, int> skillsets = new Dictionary<SkillCard.Type, int>();
+        private static SkillSet LAURA = new SkillSet(3, 2, 0, 0, 0);
+        private static SkillSet ADAMA = new SkillSet(0, 3, 2, 0, 0);
+        private static SkillSet HELO = new SkillSet(0, 2, 2, 1, 0);
+
+        
+        Dictionary<SkillCard.Type, int> skillset = new Dictionary<SkillCard.Type, int>();
+
+
+        public static SkillSet getLaura() {
+            return LAURA;
+        }
+        public static SkillSet getAdama(){
+            return ADAMA;
+        }
+        public static SkillSet getHelo(){
+            return HELO;
+        }
+
+        public int getSkilltypeAmount(SkillCard.Type type) {
+            return skillset[type];
+        }
+
+        private SkillSet(int yellow, int green, int purple, int blue, int red) {
+            skillset[SkillCard.Type.YELLOW] = yellow;
+            skillset[SkillCard.Type.GREEN] = green;
+            skillset[SkillCard.Type.PURPLE] = purple;
+            skillset[SkillCard.Type.BLUE] = blue;
+            skillset[SkillCard.Type.RED] = red;
+        }
+
+
 
     }
 }
